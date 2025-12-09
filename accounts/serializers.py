315f54c5d9126c083f4c99e34454cdcf4a480c55
Maruf <morufashiru@gmail.com>
2025-12-09 22:32:40 +0100

    Initial commit for social media API

--- accounts/serializers.py
+++ accounts/serializers.py
@@ -1,45 +1,27 @@
 from rest_framework import serializers
-from .models import User
 from django.contrib.auth import authenticate
-
-class UserSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = User
-        fields = ['id', 'username', 'bio', 'profile_picture', 'followers']
-
+from .models import User
 
 class RegisterSerializer(serializers.ModelSerializer):
     class Meta:
         model = User
-        fields = ['username', 'password']
+        fields = ['username', 'password', 'email']
         extra_kwargs = {'password': {'write_only': True}}
 
     def create(self, validated_data):
-        user = User.objects.create_user(
-            username=validated_data['username'],
-            password=validated_data['password']
-        )
-        return user
-
+        return User.objects.create_user(**validated_data)
 
 class LoginSerializer(serializers.Serializer):
     username = serializers.CharField()
-    password = serializers.CharField(write_only=True)
+    password = serializers.CharField()
 
     def validate(self, data):
-        user = authenticate(
-            username=data.get('username'),
-            password=data.get('password')
-        )
+        user = authenticate(**data)
         if not user:
-            raise serializers.ValidationError("Invalid username or password")
-        data['user'] = user
-        return data
-REST_FRAMEWORK = {
-    'DEFAULT_AUTHENTICATION_CLASSES': [
-        'rest_framework.authentication.TokenAuthentication',
-    ],
-    'DEFAULT_PERMISSION_CLASSES': [
-        'rest_framework.permissions.IsAuthenticated',
-    ]
-}
+            raise serializers.ValidationError("Invalid credentials")
+        return {"user": user}
+
+class UserSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = User
+        fields = ['id', 'username', 'email', 'bio', 'profile_picture', 'followers']
--- social_media_api/settings.py
+++ social_media_api/settings.py
@@ -38,12 +38,12 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
 
-    'accounts',
+   
     'rest_framework',
     'rest_framework.authtoken',
-]
 
-AUTH_USER_MODEL = 'accounts.User'
+    'accounts',
+]
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
@@ -126,6 +126,7 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
 AUTH_USER_MODEL = 'accounts.User'
 
 REST_FRAMEWORK = {
